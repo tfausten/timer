2ed0bd27892a7fb6c91a77fa120d79e894e76b38
tfausten <tobias.fausten@gmail.com>
2020-02-23 18:02:35 +0900

    add comments

--- timer.sh
+++ timer.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+# counter for intervals: i
 i=1
+
+# loop through intervals provided as arguments
 for interval in $@
 do
-	time_left=$interval
-	while (( $(bc -l <<< "$time_left > 0") ))
-	do
-		echo -n -e "\rInterval ${i} (${interval} minutes): ${time_left} minutes left.               "
-		if (( $(bc -l <<< "$time_left > 1") )); then wait_time=1; else wait_time=$time_left; fi
-		sleep ${wait_time}m
-		time_left=$(bc -l <<< "$time_left-$wait_time")
-	done
+    time_left=$interval
+    while (( $(bc -l <<< "$time_left > 0") ))
+    do
+	echo -n -e "\rInterval ${i} (${interval} minutes): ${time_left} minutes left.               "
 
-	echo -e "\rInterval ${i} (${interval} minutes): Done.                                                " 
-	zenity --info --text="${interval} minutes passed" --no-wrap --width=400 --height=200 --title="Interval Complete!"
-	((++i))
+	# if 1 minute or more left, wait one minute, else wait remaining time
+	if (( $(bc -l <<< "$time_left >= 1") )); then wait_time=1; else wait_time=$time_left; fi
+	sleep ${wait_time}m
+
+	# subtract wait_time from time_left
+	time_left=$(bc -l <<< "$time_left-$wait_time")
+    done
+
+    echo -e "\rInterval ${i} (${interval} minutes): Done.                                                " 
+    zenity --info --text="${interval} minutes passed" --no-wrap --width=400 --height=200 --title="Interval Complete!"
+    ((++i))
 done
 
